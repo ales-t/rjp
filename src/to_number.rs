9efbe3ea30369d91925d07cc4f9f968387d14ebc
Ales Tamchyna <ales.tamchyna@memsource.com>
2021-11-14 21:03:04 +0100

    remove missed expect() statements

--- src/to_number.rs
+++ src/to_number.rs
@@ -15,17 +15,22 @@ impl ToNumber {
 impl Processor for ToNumber {
     fn process(&mut self, mut instance: Instance) -> ProcessorResult {
         for (old_name, new_name) in &self.field_mapping {
-            let val = instance.get(old_name).expect("error").to_string();
-            let parsed_val: Value = serde_json::from_str(val.as_str()).unwrap();
+            if let Some(val) = instance.get(old_name) {
+                let parsed_val: Value = serde_json::from_str(val.to_string().as_str()).unwrap();
 
-            if !parsed_val.is_number() {
-                return ProcessorResult::Error(RjpError::BadInput(format!(
-                    "could not parse as number: {}",
-                    val
-                )));
+                if !parsed_val.is_number() {
+                    return ProcessorResult::Error(RjpError::BadInput(format!(
+                        "could not parse as number: {}",
+                        val
+                    )));
+                }
+
+                instance.insert(new_name.clone(), parsed_val);
+            } else {
+                return ProcessorResult::Error(RjpError::BadInput(
+                    format!("missing field {}", old_name)
+                ));
             }
-
-            instance.insert(new_name.clone(), parsed_val);
         }
 
         ProcessorResult::Ok(instance)
